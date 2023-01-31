21f8ec3303cc33f7f22bfb4feea599870407e7f2
dongyeop <eaea7314@naver.com>
2023-01-31 22:18:17 +0900

    feat:Update navBar

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -9,23 +9,6 @@ import Loading from "./pages/consultant/video/Loading";
 import Consultant from "./pages/consultant/Consultant";
 
 function App() {
-  // consultant 페이지에 들어갔을 때 navbar 표시를 확인 할 변수
-  const [displayNavBar, setDisplayNavBar] = useState(true);
-
-  // location을 통해 url의 값을 가져온다
-  const location = useLocation();
-
-  // useEffect로 location의 path가 변경될 때 마다 판단을 하여
-  // location.path가 consultant 페이지에 들어왔을 때 navbar의
-  // boolean형을 변형한다.
-  useEffect(() => {
-    if (location.pathname === "/consultant") {
-      setDisplayNavBar(false);
-    } else {
-      setDisplayNavBar(true);
-    }
-  }, [location.pathname]);
-
   // 대기창 설정
   // setTimeout을 이용하여 처음 5초간 Loading 페이지를 보여주고
   // 이후에 컨설턴트 페이지
@@ -40,7 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      {displayNavBar && <NavBar />}
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/auth/*" element={<Auth />} />
