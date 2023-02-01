def224c91dcdb1ae4ae2ae5a796058130192bde6
kimhyeonjin <kimhhyeonjin@gmail.com>
2023-02-01 17:53:22 +0900

    feat:Update Video component

--- Front-End/src/pages/consultant/video/PersonalColorPicker.js
+++ Front-End/src/pages/consultant/video/PersonalColorPicker.js
@@ -1,7 +1,21 @@
-const PersonalColorPicker = () => {
-  return <h1>
-    PersonalColorPicker
-  </h1>
+import Spring from '../../../assets/spring_warm.jpg';
+import Summer from '../../../assets/summer_cool.jpg';
+import Fall from '../../../assets/fall_warm.jpg';
+import Winter from '../../../assets/winter_cool.jpg';
+
+const PersonalColorPicker = (props) => {
+  return (
+    <div onMouseEnter={props.mouseEnter}>
+      <h1>봄</h1>
+      <img src={Spring} alt="spring_warm" />
+      <h1>여름</h1>
+      <img src={Summer} alt="summer_warm" />
+      <h1>가을</h1>
+      <img src={Fall} alt="fall_warm" />
+      <h1>겨울</h1>
+      <img src={Winter} alt="winter_warm" />
+    </div>
+  );
 };
 
 export default PersonalColorPicker;
--- Front-End/src/pages/consultant/video/Video.js
+++ Front-End/src/pages/consultant/video/Video.js
@@ -1,28 +1,38 @@
 import React, { Component } from "react";
 import OpenViduVideoComponent from "./OvVideo";
+import { useSelector } from "react-redux";
 import PersonalColor from "./PersonalColor";
-import "./Video.css";
+import classes from './Video.module.css';
 
-export default class UserVideoComponent extends Component {
-  // getNicknameTag() {
-  //   // Gets the nickName of the user
-  //   return JSON.parse(this.props.streamManager.stream.connection.data)
-  //     .clientData;
-  // }
+const Video = (props) => {
+  // 유저 타입 받아오기
+  const userType = useSelector((state) => state.auth.userType);
 
-  render() {
-    return (
-      <div>
-        {this.props.streamManager !== undefined ? (
-          <div className="streamcomponent">
-            <OpenViduVideoComponent streamManager={this.props.streamManager} />
+  return (
+    <div>
+      {!userType ? (
+        // 유저의 경우 퍼스널컬러 추가
+        <div className="video">
+          <div className={classes.streamcomponent}>
+            <OpenViduVideoComponent streamManager={props.streamManager} />
             <div>
               <PersonalColor />
-              {/* <p>{this.getNicknameTag()}</p> */}
             </div>
           </div>
-        ) : null}
-      </div>
-    );
-  }
-}
+        </div>
+      ) : (
+        // 컨설턴트의 경우 퍼스널컬러 없게
+        <div className="video">
+          <div className={classes.streamcomponent}>
+            <OpenViduVideoComponent streamManager={props.streamManager} />
+            <div>
+              <PersonalColor />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Video;
