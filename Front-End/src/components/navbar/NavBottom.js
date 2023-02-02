bb9d16b67e1304149738ef1e0ddb8e3509d7f439
Byeongsoo Lee <byoung1997s4@gmaail.com>
2023-02-02 15:01:05 +0900

    feat:Update Navbar 2

--- Front-End/src/components/navbar/NavBottom.js
+++ Front-End/src/components/navbar/NavBottom.js
@@ -1,11 +1,15 @@
 import { NavLink } from "react-router-dom";
-import { useState } from "react"; 
+import { useSelector } from "react-redux";
+
 
 import classes from "./NavBottom.module.css";
 
 const NavBottom = () =>{
+
+  const isLogined = useSelector((state)=> state.auth.isLogined)
+  
   return(
-    <div className={classes.bottom}>
+    <div className={(isLogined ? classes.bottom : classes.bottomhidden)}>
       {/* <NavLink to="/recommend" className={classes.LinkBottom}>추천</NavLink>  */}
       <NavLink to="/recommend" className={(navData) => navData.isActive ? classes['LinkBottom-active'] : classes.LinkBottom}>추천</NavLink> 
       <NavLink to="/consultant" className={(navData) => navData.isActive ? classes['LinkBottom-active'] : classes.LinkBottom} >컨설턴트 상담</NavLink>
