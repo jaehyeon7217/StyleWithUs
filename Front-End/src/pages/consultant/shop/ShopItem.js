48e4976d5da9f2811b267395b14e6622a4f9039c
이동엽 <eaea7314@naver.com>
2023-02-02 11:01:15 +0900

    feat:Update axios

--- Front-End/src/pages/consultant/shop/ShopItem.js
+++ Front-End/src/pages/consultant/shop/ShopItem.js
@@ -1,12 +1,15 @@
 import { Fragment } from "react";
 
+import classes from "./ShopItem.module.css";
+
 const ShopItem = (props) => {
   // item : no, imgLink, maker, link, title, afterPrice;
   const item = props.item;
   return (
     <Fragment>
-      <span>{item.no}</span>
-      <span>{item.title}</span>
+      <div className={classes.index}>{item.no}.</div>
+      <img src={item.imgLink} alt="item-img" className={classes.img}/>
+      <div className={classes.title}>{item.title}</div>
     </Fragment>
   );
 };
--- Front-End/src/pages/consultant/shop/ShopItems.js
+++ Front-End/src/pages/consultant/shop/ShopItems.js
@@ -1,9 +1,12 @@
-import { Fragment, useState } from "react";
-import { useSelector } from "react-redux";
+import { Fragment, useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { shopActions } from "../../../store/shop";
+import axios from "axios";
 
 import ShopItem from "./ShopItem";
 import Pagination from "../component/Pagination";
 import classes from "./ShopItems.module.css";
+import codeNumber from "./CodeNumber";
 
 const ShopItems = (props) => {
   const [pagination, setPagination] = useState(1);
@@ -12,32 +15,51 @@ const ShopItems = (props) => {
   const type = props.detailPage.type;
   const detail = props.detailPage.detail;
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const url = `https://i8d105.p.ssafy.io/be/data/${codeNumber[type][detail]}`;
+
+    axios
+    .get(url)
+      .then((response) => {
+        const payload = {type, detail, data: response.data.data}
+        dispatch(shopActions.downloadData(payload));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   // 리덕스에서 옷 정보를 가져온다.
-  const items = useSelector(state => state.shop.category)
+  const items = useSelector((state) => state.shop.category);
+
+  // 페이지에 출력될 옷 개수
+  const clothesCount = 5;
 
   // 페이지 마지막 번호
-  const lastPageNumber = Math.floor(items[type][detail].length / 10);
+  const lastPageNumber = Math.floor(items[type][detail].length / clothesCount);
 
   // 페이지 번호를 담은 배열
   let pageArray = new Array();
   for (let i = 0; i < lastPageNumber; i++) {
     pageArray.push(i + 1);
-  };
+  }
 
   // 페이지네이션의 마지막 번호
   const lastDivPageNumber = Math.ceil(lastPageNumber / 5);
 
   // 페이지 버튼을 클릭시 pagination을 업로드 하는 함수
-  const onClickHandler = (num) => {    
+  const onClickHandler = (num) => {
     setPagination(num);
-  }
+  };
 
   // 페이지네이션을 뒤로 가게 하는 버튼
   const prevPagination = () => {
     if (divPagination > 1) {
       setDivPageNation((prevState) => {
         return prevState - 1;
-      })
+      });
     }
   };
 
@@ -45,24 +67,44 @@ const ShopItems = (props) => {
     if (divPagination < lastDivPageNumber) {
       setDivPageNation((prevState) => {
         return prevState + 1;
-      })
+      });
     }
   };
 
   return (
     <Fragment>
-      <h3>{detail}</h3>
-      <ul>
+      <h3 className={classes.h3}>{detail}</h3>
+      <ul className={classes.ul}>
         {items[type][detail].map((item, idx) => {
-          return (10 * (pagination - 1)) <= idx && (10 * (pagination - 1)) + 10 > idx ? <li key={idx}><ShopItem item={item}/></li> : "";
+          return clothesCount * (pagination - 1) <= idx &&
+            clothesCount * (pagination - 1) + clothesCount > idx ? (
+            <li key={idx} className={classes.li}>
+              <ShopItem item={item} />
+            </li>
+          ) : (
+            ""
+          );
         })}
       </ul>
-      <div>
-        <span onClick={prevPagination}>prev</span>
-        {pageArray.map((pageNumber, idx) => {
-          return (5 * (divPagination - 1)) <= idx && (5 * (divPagination - 1)) + 5 > idx ? <Pagination key={`page-${pageNumber}`} pageNumber={pageNumber} onClickHandler={onClickHandler}/> : ""; 
-        })}
-        <span onClick={nextPagination}>next</span>
+      <div className={classes.pagination}>
+        <span onClick={prevPagination} className={"material-symbols-outlined " + classes.prev }>
+          arrow_back_ios_new
+        </span>
+        <div className={classes.bindNumber}>
+          {pageArray.map((pageNumber, idx) => {
+            return 5 * (divPagination - 1) <= idx &&
+              5 * (divPagination - 1) + 5 > idx ? (
+              <Pagination
+                key={`page-${pageNumber}`}
+                pageNumber={pageNumber}
+                onClickHandler={onClickHandler}
+              />
+            ) : (
+              ""
+            );
+          })}
+        </div>
+        <span id="next" onClick={nextPagination} className={"material-symbols-outlined " + classes.next}>arrow_forward_ios</span>
       </div>
     </Fragment>
   );
