f6430f328df9397be1c8b3142827706e4e72b9e1
이동엽 <eaea7314@naver.com>
2023-02-07 15:15:45 +0900

    feat:Create MainPage

--- Front-End/src/pages/Home/Home.js
+++ Front-End/src/pages/Home/Home.js
@@ -1,14 +1,37 @@
-import LogoImage from "../../assets/logo.png"
+import { Fragment } from "react";
+
+import ButtonBox from "./ButtonBox";
 import classes from "../Home/Home.module.css"
 
-
 const Home = () =>{
-  return(
-    <div className={classes.HomePage}>
-      <img src={LogoImage} alt="img" />
-      <p>Welcome to Style With Us</p>
-    </div>
-  )
+
+  const onClickHandler = () => {
+    document.querySelector(`.${classes['section'+3]}`).scrollIntoView({behavior: "smooth", block: "center"});
+  };
+
+  const TopMoveHandler = () => {
+    document.querySelector(`.${classes['section'+1]}`).scrollIntoView({behavior: "smooth", block: "center"});
+  }
+
+  return (
+    <Fragment>
+      <ButtonBox />
+      <div className={classes['section1']}>
+        <h1>Style<br/>With<br/>Us</h1>
+        <button onClick={onClickHandler}>이동</button>
+      </div>
+      <div className={classes['section2']}>
+        <h1>Style<br/>With<br/>Us</h1>
+      </div>
+      <div className={classes['section3']}>
+        <h1>Style<br/>With<br/>Us</h1>
+      </div>
+      <div className={classes['section4']}>
+        <h1>Style<br/>With<br/>Us</h1>
+      </div>
+      <button className={classes.top} onClick={TopMoveHandler}>위로이동</button>
+    </Fragment>
+  );
 }
 
 export default Home
