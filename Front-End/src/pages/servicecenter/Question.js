b3becfd57103f67837528d2577dab54cec22f286
양서정 <seojeong4560@daum.net>
2023-02-07 17:48:28 +0900

    feat:Add footer1

--- Front-End/src/pages/servicecenter/Question.js
+++ Front-End/src/pages/servicecenter/Question.js
@@ -9,7 +9,7 @@ const Question = (props) => {
             <div className={classes.QItem} onClick={()=>{setVisible(!visible)}}>
                 <h3 className={classes.category}>{props.category}</h3>
                 <h3 className={classes.Title}>{props.title}</h3>
-                <div>
+                <div className={classes.crampsbtn}>
                     {visible ? <div className={classes.checkIconTwo}></div>: <div className={classes.checkIcon}></div>}
                 </div>
             </div>
@@ -21,6 +21,7 @@ const Question = (props) => {
                         <div className={classes.QdapHard}>{props.contentHard}</div>
                         <div className={classes.QdapLight}>{props.content2}</div>
                         <div className={classes.QdapLight}>{props.content1}</div>
+                        <div className={classes.QdapLight}>{props.content3}</div>
                     </div>
             </div>}
             </div>
