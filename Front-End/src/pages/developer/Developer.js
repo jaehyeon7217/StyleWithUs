30f8ce29c627758cd2627c5af32af21f1514ba60
양서정 <seojeong4560@daum.net>
2023-02-07 22:40:04 +0900

    feat:Add developer1

---
+++ Front-End/src/pages/developer/Developer.js
@@ -0,0 +1,23 @@
+import classes from './Developer.module.css'
+
+const Developer = () => {
+    return(
+        <div>
+            <div>
+                <h1>DEVELOPER</h1>
+                <h3>스타일 혁신을 일군 여섯명의 개발자와</h3>
+                <h3>스타일 윗 어스의 개발 일지를 소개합니다.</h3>
+            </div>
+            <div>
+                
+            </div>
+            <div>
+                
+            </div>
+            <h3>STYLE WITH US DIARY</h3>
+            <h1>개발 일기</h1>
+        </div>
+    )
+}
+
+export default Developer;
