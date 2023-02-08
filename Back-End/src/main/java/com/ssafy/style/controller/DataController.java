d13d093ba0ea37c9e9f1e0f046b9f135be049c65
박재현 <jhp1276@naver.com>
2023-02-08 10:43:21 +0900

    feat:Back-consultant_approval

--- Back-End/src/main/java/com/ssafy/style/controller/DataController.java
+++ Back-End/src/main/java/com/ssafy/style/controller/DataController.java
@@ -2,6 +2,7 @@ package com.ssafy.style.controller;
 
 
 import com.ssafy.style.data.dto.DataDto;
+import com.ssafy.style.data.dto.UserDto;
 import com.ssafy.style.service.DataService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -13,10 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/data")
@@ -34,7 +32,7 @@ public class DataController {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     // 무신사 Item 정보 크롤링
-    @GetMapping(value = "{gender}/{no}")
+    @GetMapping(value = "consulting/{gender}/{no}")
     @ApiOperation(value = "무신사 Item 정보 크롤링")
     public ResponseEntity<?> getDataTest(
             @PathVariable @ApiParam(value = "Item 카테고리 number", required = true) String no,
@@ -67,7 +65,39 @@ public class DataController {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
+    }
 
+    @PostMapping(value = "/recommend")
+    @ApiOperation(value = "추천 상품")
+    public ResponseEntity<?> recomemendItem (@RequestBody @ApiParam UserDto userInfo){
+        logger.info("Call the recommendItem Methods");
+
+        Map<String, Object> map = new HashMap<>();
+//        int[] no = {001, 003, 002, 005};
+
+        if(userInfo.getUserHeight() == null)
+            return  ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+
+        try{
+            List<DataDto> list = new LinkedList<>();
+            list = dataService.getCommnedTopItem(userInfo);
+            map.put("top", list);
+            list.clear();
+            list = dataService.getCommendBottomItem(userInfo);
+            map.put("bottom", list);
+            list.clear();
+            list = dataService.getCommendOuterItem(userInfo);
+            map.put("outer", list);
+            list.clear();
+            list = dataService.getCommendShoesItem(userInfo);
+            map.put("shoes", list);
+
+            map.put("msg", "success");
+            return ResponseEntity.status(HttpStatus.OK).body(map);
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
 
     }
 
