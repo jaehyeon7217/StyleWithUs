784563e4d27bbecfa36272f301f921c816b2786b
박재현 <jhp1276@naver.com>
2023-02-08 13:15:02 +0900

    feat:back-user change

--- Back-End/src/main/java/com/ssafy/style/service/impl/DataServiceImpl.java
+++ Back-End/src/main/java/com/ssafy/style/service/impl/DataServiceImpl.java
@@ -17,51 +17,10 @@ public class DataServiceImpl implements DataService {
 
     @Override
     public List<DataDto> getData(String no) {
+
         String URL = "https://www.musinsa.com/categories/item/" + no; // 원하는 url
-        Document doc;
-        List<DataDto> list = new ArrayList<>();
 
-        try {
-            doc = Jsoup.connect(URL)
-                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
-                    .get(); // 원하는 url에서 전체 구조를 받아온다
-
-            if(doc.select("#searchList > li:nth-child(1)").toString().equals(""))
-                return null;
-
-            for(int i=1;i<=90;i++) {
-                DataDto data = new DataDto();
-
-                data.setNo(i);
-
-                Elements el = doc.select("#searchList > li:nth-child(" + i + ") > div.icon_new");
-                if(!el.text().equals(""))
-                    data.setSale(el.text());
-
-                data.setImgLink(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.list_img > a > img").attr("abs:data-original"));
-                data.setMaker(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.item_title > a").text());
-                data.setLink(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.list_info > a").attr("abs:href"));
-                data.setTitle(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.list_info > a").text());
-
-                el = doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.price > del");
-                if(!el.text().equals("")){
-                    data.setBeforePrice(el.text());
-                    data.setAfterPrice(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.price").text().split(" ")[1]);
-                }
-                else
-                    data.setAfterPrice(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.price").text());
-
-
-                list.add(data);
-            }
-
-            return list;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        return startData(URL, 1);
 
     }
 
@@ -111,22 +70,90 @@ public class DataServiceImpl implements DataService {
     }
 
     @Override
-    public List<DataDto> getCommnedTopItem(UserDto userInfo) {
-        return null;
+    public List<DataDto> getCommendTopItem(UserDto userInfo) {
+
+        int should = userInfo.getUserShoulder();
+        int chest = userInfo.getUserChest();
+        int sleeve = userInfo.getUserSleeve();
+
+        String URL = "https://www.musinsa.com/categories/item/001?measure=" +
+                "measure_3^" + (should - 2) + "^" + (should + 2) +
+                ",measure_1^" + (chest - 2) + "^" + (chest + 2) +
+                ",measure_2^" + (sleeve - 2) + "^" + (sleeve + 2);
+
+        return startData(URL, userInfo.getUserGender());
     }
 
     @Override
     public List<DataDto> getCommendBottomItem(UserDto userInfo) {
+
+        String URL = "https://www.musinsa.com/categories/item/003?"; // 원하는 url
+
         return null;
     }
 
     @Override
     public List<DataDto> getCommendOuterItem(UserDto userInfo) {
+
+        String URL = "https://www.musinsa.com/categories/item/002?"; // 원하는 url
+
         return null;
     }
 
     @Override
     public List<DataDto> getCommendShoesItem(UserDto userInfo) {
+
+        String URL = "https://www.musinsa.com/categories/item/005?"; // 원하는 url
+
+        return null;
+    }
+
+    public List<DataDto> startData(String URL, int gender){
+
+        Document doc;
+        List<DataDto> list = new ArrayList<>();
+
+        try {
+            doc = Jsoup.connect(URL)
+                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
+                    .get(); // 원하는 url에서 전체 구조를 받아온다
+
+            if(doc.select("#searchList > li:nth-child(1)").toString().equals(""))
+                return null;
+
+            for(int i=1;i<=90;i++) {
+                if(doc.select("#searchList > li:nth-child("+i+")").toString().equals(""))
+                    break;
+                DataDto data = new DataDto();
+
+                data.setNo(i);
+
+                Elements el = doc.select("#searchList > li:nth-child(" + i + ") > div.icon_new");
+                if(!el.text().equals(""))
+                    data.setSale(el.text());
+
+                data.setImgLink(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.list_img > a > img").attr("abs:data-original"));
+                data.setMaker(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.item_title > a").text());
+                data.setLink(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.list_info > a").attr("abs:href"));
+                data.setTitle(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.list_info > a").text());
+
+                el = doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.price > del");
+                if(!el.text().equals("")){
+                    data.setBeforePrice(el.text());
+                    data.setAfterPrice(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.price").text().split(" ")[1]);
+                }
+                else
+                    data.setAfterPrice(doc.select("#searchList > li:nth-child("+i+") > div.li_inner > div.article_info > p.price").text());
+
+
+                list.add(data);
+            }
+
+            return list;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
